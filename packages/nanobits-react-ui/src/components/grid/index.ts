3a6767b0d9407d6dbc9ffe69dd5d7804bff91e6f
Tinkeshwar <tinkeshwar@gmail.com>
2022-08-13 22:54:08 +0530

    1.0.13

--- packages/nanobits-react-ui/src/components/grid/index.ts
+++ packages/nanobits-react-ui/src/components/grid/index.ts
@@ -1,6 +1,8 @@
 import { Col } from './Col'
 import { Container } from './Container'
 import { Div } from './Div'
+import { Heading } from './Heading'
+import { Paragraph } from './Paragraph'
 import { Row } from './Row'
 
-export { Col, Container, Div, Row }
+export { Col, Container, Div, Row, Heading, Paragraph }
