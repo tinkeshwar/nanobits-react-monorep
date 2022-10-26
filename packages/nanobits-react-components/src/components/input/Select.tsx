82575d4a4cee20e78be69be4e3b320b8b359fa7e
Tinkeshwar Singh <tinkeshwar@gmail.com>
2022-10-26 20:23:17 +0530

    single select dropdown

--- packages/nanobits-react-components/src/components/input/Select.tsx
+++ packages/nanobits-react-components/src/components/input/Select.tsx
@@ -1,9 +1,9 @@
 import React, { forwardRef, InputHTMLAttributes, useEffect, useRef, useState } from "react";
 import { FormSelectProps } from "nanobits-react-ui/components/form/FormSelect";
 import { Label, Prefix, Suffix } from "../label";
-import { InputGroup, FormFeedback, FormInput, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from "nanobits-react-ui";
+import { InputGroup, FormFeedback } from "nanobits-react-ui";
 import classNames from "classnames";
-import { TextInput } from "./Text";
+import { FormSelect } from "nanobits-react-ui";
 
 export interface SelectOptionProps {
     value: string
@@ -16,7 +16,7 @@ export interface SearchOptionProps {
     text?: string
 }
 
-export interface SelectInputProps extends InputHTMLAttributes<HTMLInputElement>{
+export interface SelectInputProps extends InputHTMLAttributes<HTMLSelectElement> {
     className?: string
     label?: string
     required?: boolean
@@ -30,12 +30,11 @@ export interface SelectInputProps extends InputHTMLAttributes<HTMLInputElement>{
     error?: string
     requiredText?: string
     disabled?: boolean
-    onUpdate?: (value: SelectOptionProps) => void
-    options: SelectOptionProps[],
-    search?: SearchOptionProps
+    onUpdate?: (value: string) => void
+    options: SelectOptionProps[]
 }
 
-export const SelectInput = forwardRef<HTMLInputElement, SelectInputProps & FormSelectProps>((
+export const SelectInput = forwardRef<HTMLSelectElement, SelectInputProps & FormSelectProps>((
     {
         className,
         label,
@@ -52,69 +51,55 @@ export const SelectInput = forwardRef<HTMLInputElement, SelectInputProps & FormS
         requiredText,
         disabled,
         onUpdate,
-        search,
         ...rest
     },
     ref
-)=>{
-    
+) => {
+
     const _className = classNames(
         'n-custom-select-input-class',
         className
     )
 
-    const [errorMessage, setErrorMessage] = useState<string|undefined>(error)
-    const [seletedValue, setSelectedValue] = useState<string|undefined>(value)
+    const [errorMessage, setErrorMessage] = useState<string | undefined>(error)
 
-    const handleChange = (item: SelectOptionProps) => {
-        if (onUpdate) {
-            setSelectedValue(item.label)
-            return onUpdate(item)
-        }
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        if (onUpdate) return onUpdate(event.target.value)
         throw new Error('Provide `onUpdate` to input component')
     }
 
     const errorExecuted = useRef(true)
     useEffect(() => {
-        if(errorExecuted.current){
+        if (errorExecuted.current) {
             errorExecuted.current = false
             error ? setErrorMessage(error) : setErrorMessage(undefined)
         }
-    },[error])
+    }, [error])
 
     return (
         <React.Fragment>
             {label && <Label labelfor={name} required={required} label={label} />}
-            <Dropdown variant={'dropdown'} className={'w-100'}>
-                <DropdownToggle color={'none'} className={'d-block w-100 p-0 border-0 text-left bg-white h6 mb-0 dropdown-toggler-select'}>
-                    <InputGroup>
-                        {(iconLeft || textLeft) && <Prefix icon={iconLeft} text={textLeft} required={required} />}
-                        <FormInput
-                            className={_className}
-                            ref={ref}
-                            id={`for-${name}`}
-                            type={'text'}
-                            value={seletedValue}
-                            name={name}
-                            placeholder={placeholder}
-                            aria-describedby={name}
-                            invalid={errorMessage ? true : false}
-                            required={required}
-                            feedbackInvalid={requiredText}
-                            readOnly={true}
-                            {...rest}
-                        />
-                        {errorMessage && <FormFeedback>{errorMessage}</FormFeedback>}
-                        {(iconRight || textRight) && <Suffix icon={iconRight} text={textRight} required={required} />}
-                    </InputGroup>
-                </DropdownToggle>
-                {(options && options.length) && <DropdownMenu className={'pt-0'}>
-                    {search && <DropdownItem className={'p-0'}><TextInput iconLeft={search.icon||'fa-magnifying-glass'} name={'search'} placeholder={search.text||'search...'}/></DropdownItem>}
-                    {options.map((option: SelectOptionProps, index: number)=> {
-                        return <DropdownItem onClick={()=>handleChange(option)} key={`select-option-${index}`} disabled={option.disabled}>{option.label}</DropdownItem>
-                    })}
-                </DropdownMenu>}
-            </Dropdown>
+            <InputGroup>
+                {(iconLeft || textLeft) && <Prefix icon={iconLeft} text={textLeft} required={required} />}
+                <FormSelect
+                    className={_className}
+                    ref={ref}
+                    id={`for-${name}`}
+                    value={value}
+                    name={name}
+                    placeholder={placeholder}
+                    aria-describedby={name}
+                    required={required}
+                    disabled={disabled}
+                    feedbackInvalid={requiredText}
+                    invalid={errorMessage ? true : false}
+                    onChange={handleChange}
+                    options={options}
+                    {...rest}
+                />
+                {errorMessage && <FormFeedback>{errorMessage}</FormFeedback>}
+                {(iconRight || textRight) && <Suffix icon={iconRight} text={textRight} required={required} />}
+            </InputGroup>
         </React.Fragment>
     )
 })
